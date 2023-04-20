8929ad0f217b847811ae796a2e9af5e269b7460a
Max Rogers <>
2023-04-20 10:28:35 -0400

    Add Hello World Xcode Project

---
+++ Scuff iOS/Scuff/ScuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScuffApp.swift
+//  Scuff
+//
+//  Created by Max Rogers on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ScuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
