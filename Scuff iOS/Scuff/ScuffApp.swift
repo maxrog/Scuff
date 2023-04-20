d4792da3545c564780fabeb8d401c6d3ff840033
Max Rogers <>
2023-04-20 10:48:05 -0400

    Basic directory structure

--- Scuff iOS/Scuff/ScuffApp.swift
+++ Scuff iOS/Scuff/ScuffApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct ScuffApp: App {
+    
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some Scene {
         WindowGroup {
+            // TODO Tab View
             ContentView()
+        }
+        .onChange(of: scenePhase) { newScenePhase in
+            
+            switch scenePhase {
+            case .active:
+                /// application became active
+                break
+            case .background:
+                /// application went to background
+                break
+            case .inactive:
+                /// application became inactive
+                break
+            default:
+                break
+            }
         }
     }
 }
+
